31df0a3704a84ee21c44f6686fc37e639c8c1d4c
yanghengjian <770129076@qq.com>
2018-12-19 14:52:37 +0800

    之后提交了pom.xml还有apliction.xml配置文件

--- src/main/java/com/tt/teach/dao/StudentDao.java
+++ src/main/java/com/tt/teach/dao/StudentDao.java
@@ -1,4 +1,10 @@
 package com.tt.teach.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
 public interface StudentDao {
+
 }
